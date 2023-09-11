a4e922444bcf4fbe5b1dbffe00071084c02186d8
Mahendra Suthar <mahendrasuthar@sharechat.co>
2023-09-12 00:56:27 +0530

    add link to vs code

--- src/helpers/html.js
+++ src/helpers/html.js
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 
 function createHTMLTableRow(data, idx) {
-  return `<tr>
+  return `
+  <tr>
       <td>${idx}.</td>
       <td>
         <a href="vscode://file${data.file}">${data.fileName}</a>
@@ -15,7 +16,7 @@ function createHTMLTableRow(data, idx) {
           100
         ).toFixed(2)
       }%</td>
-    </tr>`;
+  </tr>`;
 }
 
 function generateHTMLReport(
@@ -28,7 +29,9 @@ function generateHTMLReport(
     .map((value) => {
       return `
       <div class="change-set"> 
-      <h3>${value.file}</h3>
+      <h3>
+        <a href="vscode://file${value.file}">${value.fileName}</a>
+      </h3>
       <div class="file-stat" >
         <p>Atomic classes found: ${value.atomicClassesCount}</p>
         <p>Classes transformed: ${value.classesTransformedCount}</p>
@@ -41,10 +44,16 @@ function generateHTMLReport(
       <table class="changes-table">
         ${_.uniq(value.transformedClasses).join('')}
       </table>
+      ${
+        value.notTransformedClasses.length > 0
+          ? `
+      
       <h4>Not transformed classes:</h4>
       <table class="changes-table">
         ${_.uniq(value.notTransformedClasses).join('')}
-      </table>
+      </table>`
+          : ''
+      }
       </div>
   `;
     });
@@ -58,55 +67,59 @@ function generateHTMLReport(
   ).join('');
 
   const html = `
+  <html>
+  <head>
+    <title>Transform report</title>
+  </head>
   <style>
-  body {
-    padding: 16px;
-  }
-  * {
-    font-family: monospace;
-  }
-  
-  table {
-    width: 100%;
-  }
+    body {
+      padding: 16px;
+    }
 
-  table, th, td {
-    border: 1px solid black;
-    border-collapse: collapse;
-    text-align: left;
-  }
+    * {
+      font-family: monospace;
+    }
+    
+    table {
+      width: 100%;
+    }
 
-  td, th {
-    padding: 8px 16px;
-  }
+    table, th, td {
+      border: 1px solid black;
+      border-collapse: collapse;
+      text-align: left;
+    }
 
-  .change-list {
-    margin-top: 1rem;
-  }
+    td, th {
+      padding: 8px 16px;
+    }
 
-  .change-set {
-    margin-bottom: 0 1rem;
-    border: 1px solid black;
-    padding: 0 1rem;
-    border-radius: 10px;
-  }
+    .change-list {
+      margin-top: 1rem;
+    }
 
-  .file-stat {
-    display: flex;
-    background-color: aliceblue;
-  }
+    .change-set {
+      margin: 0rem 0rem 1rem 0rem;
+      border: 1px solid black;
+      padding: 0 1rem 1rem 1rem;
+      border-radius: 10px;
+    }
 
-  .file-stat p {
-    margin: 0;
-    border: 1px solid black;
-    flex: 1;
-    padding: 0.5rem;
-  }
+    .file-stat {
+      display: flex;
+      background-color: aliceblue;
+    }
 
-  .changes-table {
-    width: fit-content
-  }
- 
+    .file-stat p {
+      margin: 0;
+      border: 1px solid black;
+      flex: 1;
+      padding: 0.5rem;
+    }
+
+    .changes-table {
+      width: fit-content
+    }
   </style>
   <h2>Transform report</h2>
   <h3>Files transformed: ${transformReport.length}</h3>
